c56dbb1038027f3f6f879b744e0a872895ecb15b
João Henrique Medeiros Proença <joao.h.proenca@gmail.com>
2025-09-18 00:28:44 -0300

    ex015 iniciando

---
+++ aula012ex/ex015/script.js
@@ -0,0 +1,9 @@
+const data = new Date
+const ano = data.getFullYear()
+const btn = document.querySelector ('#verificar')
+const nascimento = document.querySelector ('txtano')
+
+
+btn.addEventListener('click', function(){
+    document.querySelector('#res').innerHTML = 
+})

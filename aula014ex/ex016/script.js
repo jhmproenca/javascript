fcb88c95a3095c02de798addb331f93b1d233cb6
João Henrique Medeiros Proença <joao.h.proenca@gmail.com>
2025-09-23 00:00:47 -0300

    finalizando ex016

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -1,13 +1,31 @@
-let btn = document.querySelector('#contar')
-
+let btn = document.getElementById('contar')
 let res = document.querySelector('#res')
 
 btn.addEventListener('click', function  () {
-    let inicio = Number(document.querySelector('#inicio').value)
-    let fim = Number(document.querySelector('#fim').value)
-    let passo = Number(document.querySelector('#passo').value)
-    for (let c = inicio; c <= fim; c = c + passo) {
-        res.innerHTML = c
-        
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            alert('Passo inválido! Considerando Passo = 1')
+            p = 1
+        }
+        if (i < f){
+            for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} - `
+            }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} - `
+            }
+        }
+        res.innerHTML += `Fim!`
     }
 })
